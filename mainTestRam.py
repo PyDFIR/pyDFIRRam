7b12640b4b7e0f9f4b1a3ed6ebdafcc0f965fb5c
Braguette <alexis.debrito@ecole2600.com>
2023-07-20 16:35:38 -0400

    [-] remove main

--- mainTestRam.py
+++
@@ -1,7 +0,0 @@
-#from pyDFIRLogs import *
-from pyDFIRRam import windows
-import pandas
-import os
-file = "/home/remnux/Desktop/project/2600/memdump.mem"
-windows_obj = windows(file,showConfig=True,Outputformat="dataframe",outpath=os.getcwd(),savefile=False)
-print(windows_obj.CmdLine)
