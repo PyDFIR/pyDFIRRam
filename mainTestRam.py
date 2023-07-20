f6e04ee357c3d28c5f816c862ab3230fa35f1608
Braguette <alexis.debrito@ecole2600.com>
2023-07-20 11:38:04 -0400

    main

--- mainTestRam.py
+++ mainTestRam.py
@@ -3,32 +3,29 @@ from pyDFIRRam import windows
 import pandas
 file = "/home/remnux/Desktop/project/2600/memdump.mem"
 windows_obj = windows(file,showConfig=True,Outputformat="dataframe")
-#c= windows_obj.HiveList()
-#print(c)
-#def unique(data):
-#    ret = []
-#    for e in data:
-#        if not e["ForeignAddr"] in ret:
-#            ret.append(e["ForeignAddr"])
-#    return ret
-#
-#### Create an object
-#file = "/home/remnux/Desktop/project/2600/memdump.mem"
-#windows_obj = windows(file,showConfig=True,Outputformat="dataframe")
-#ns = windows_obj.NetScan()
-#print(pandas.DataFrame(ns).head())
-##print(unique(ns))
-#print(windows_obj.CmdLine())
-#print(windows_obj.Info())
-#print(windows_obj.NetScan)
-#hash = windows_obj.get_hash(file)
-#print(hash)
-#t =windows_obj.FileScan()
-#pd = pandas.DataFrame(t)
-#print(pd)
-#value_to_find = "Mft"
-#print(pd[pd['Name'].str.contains(value_to_find)])
-#l = windows_obj.DumpFiles([0xcf0c25ee2210])
+c= windows_obj.HiveList()
+print(c)
+def unique(data):
+    ret = []
+    for e in data:
+        if not e["ForeignAddr"] in ret:
+            ret.append(e["ForeignAddr"])
+    return ret
+
+### Create an object
+file = "/home/remnux/Desktop/project/2600/memdump.mem"
+ns = windows_obj.NetScan()
+print(pandas.DataFrame(ns).head())
+#print(unique(ns))
+print(windows_obj.CmdLine())
+print(windows_obj.Info())
+print(windows_obj.NetScan)
+t =windows_obj.FileScan()
+pd = pandas.DataFrame(t)
+print(pd)
+value_to_find = "Mft"
+print(pd[pd['Name'].str.contains(value_to_find)])
+l = windows_obj.DumpFiles([0xcf0c25ee2210])
 
 
 ### test Function ok
@@ -64,7 +61,7 @@ windows_obj = windows(file,showConfig=True,Outputformat="dataframe")
 
 #### Function not OK
 #print("LsaDump",windows_obj.LsaDump())
-print("skeleton_key_check",windows_obj.skeleton_key_check())
+#print("skeleton_key_check",windows_obj.skeleton_key_check())
 #print("Memmap",windows_obj.Memmap())
 #print("CacheDump",windows_obj.CacheDump())
 #print("YaraScan",windows_obj.YaraScan())
