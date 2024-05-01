5ef35641b790ca1987d75eb110609dc2ea45b24b
std3 <67806187+standard3@users.noreply.github.com>
2024-05-01 20:59:23 +0200

    feat: added base plugin resolver

--- pydfirram/core/base.py
+++ pydfirram/core/base.py
@@ -1,50 +1,84 @@
 """todo"""
 
 from enum import Enum
-from typing import List, Dict
+from typing import List, Dict, Type
+from dataclasses import dataclass
 
-from volatility3 import framework, plugins
+from volatility3 import framework, plugins, interfaces
+
 
 class OperatingSystem(Enum):
-    """Represents an operating system."""
-    WINDOWS = 1
-    LINUX = 2
-    MACOS = 3
+    """Represents a supported operating system."""
+
+    WINDOWS = "windows"
+    LINUX = "linux"
+    MACOS = "mac"
+
+    @staticmethod
+    def to_list() -> List[str]:
+        """Returns a list of supported operating systems."""
+        return [os.value for os in OperatingSystem]
+
+
+class PluginType(Enum):
+    """Represents a plugin type."""
+
+    GENERIC = 1
+    SPECIFIC = 2
+
+
+@dataclass
+class PluginEntry:
+    """Represents a plugin entry."""
+
+    type: PluginType
+    name: str
+    path: str
+
+    def __repr__(self) -> str:
+        return f"PluginEntry({self.type}, {self.name}, {self.path})"
+
 
 class Generic:
     """Represents a generic OS to be parsed by volatility."""
 
-    def __init__(self, os: OperatingSystem):#, profile: str, image: str):
+    def __init__(self, os: OperatingSystem):  # , profile: str, image: str):
         self.os = os
-        self.plugins: List
+        self.plugins: List[PluginEntry] = []
 
     def __str__(self):
         return f"Generic OS: {self.os.name}"
 
-    # def init_commands(self) -> None:
-    #     """Parses the available commands for the OS."""
-    #     pass
-
-    def get_plugins_list(self) -> List[str]:
+    def get_plugins_list(self) -> Dict[str, Type[interfaces.plugins.PluginInterface]]:
         """Returns a list of available plugins for the OS."""
-        failures = framework.import_files(plugins, True)
+        framework.import_files(plugins, True)
         plugin_list = framework.list_plugins()
 
         return plugin_list
-    def create_index_plugins(self) -> dict:
+
+    def parse_plugins_list(self) -> List[PluginEntry]:
         """
-            Parsing of get_plugins
+        Parsing of get_plugins
         """
-        plugin_list : List[str] = self.get_plugins_list()
-        for e in plugin_list:
-            value : list = e.split(".")
-            print("--------------------------------------------------")
-            print(e)
-            if value[0] == "volatility3":
-                #print(f"plateform: {value[4]}\nPlugins: {value[-1]}")
-                print(str.lower(self.os))
-                if value[4] == str.lower(self.os) :
-                    print(f'{e}')
+        plugin_list = self.get_plugins_list()
+
+        parsed: List[PluginEntry] = list()
+
+        for plugin in plugin_list:
+            elements = plugin.split(".")
+            platform = elements[0]
+            path = ".".join(elements[1:-1])
+            name = elements[-1]
+
+            if platform not in OperatingSystem.to_list():
+                plugin = PluginEntry(PluginType.GENERIC, name, platform)
+
+            elif platform == self.os.value:
+                plugin = PluginEntry(PluginType.SPECIFIC, name, path)
+
             else:
-                if value[4] == str.lower(self.os) :
-                    print(f'{e}')
+                continue
+
+            parsed.append(plugin)  # type: ignore
+
+        return parsed
