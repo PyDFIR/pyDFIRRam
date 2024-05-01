fb30628729792a146a99427ceb0f4175ae0d4fd4
std3 <67806187+standard3@users.noreply.github.com>
2024-05-02 00:05:11 +0200

    feat: added context functions

--- pydfirram/core/base.py
+++ pydfirram/core/base.py
@@ -1,5 +1,7 @@
 """todo"""
 
+import os
+
 from enum import Enum
 from typing import List, Dict, Any
 from dataclasses import dataclass
@@ -7,6 +9,13 @@ from pathlib import Path
 
 from loguru import logger
 from volatility3 import framework, plugins
+from volatility3.framework import interfaces, contexts, automagic
+from volatility3.framework.plugins import construct_plugin
+
+# TODO:
+# get the plugin interface in the plugin entry from the parsing
+# finish the build function in context
+# wrap the run plugin through the context
 
 
 class OperatingSystem(Enum):
@@ -35,10 +44,75 @@ class PluginEntry:
 
     type: PluginType
     name: str
-    path: str
+    interface: interfaces.plugins.PluginInterface
 
     def __repr__(self) -> str:
         return f"PluginEntry({self.type}, {self.name}, {self.path})"
+
+    def get_full_path(self, operating_system: OperatingSystem) -> str:
+        """Returns the full path of the plugin."""
+        return f"{operating_system.value}{self.path}.{self.name}"
+
+    def get_plugin_interface(self) -> interfaces.plugins.PluginInterface:
+        """Returns the plugin interface."""
+        return plugins.get_plugin(self.get_full_path())
+
+
+class Context:
+    """Represents a context for a plugin."""
+
+    KEY_LAYER_STACKER = "automagic.LayerStacker"
+    KEY_STACKERS = f"{KEY_LAYER_STACKER}.stackers"
+    KEY_SINGLE_LOCATION = f"{KEY_LAYER_STACKER}.single_location"
+
+    def __init__(
+        self,
+        operating_system: OperatingSystem,
+        dump_file: Path,
+        plugin: PluginEntry,
+    ):
+        """Initializes a context."""
+        self.os = operating_system
+        self.dump_file = dump_file
+        self.context = contexts.Context()
+        self.plugin = plugin
+
+    # def __setattr__(self, name: str, value: Any) -> None:
+    #     """Set an attribute."""
+    #     setattr(self.context, name, value)
+
+    # def __getattr__(self, name: str) -> Any:
+    #     """Get an attribute."""
+    #     return getattr(self.context, name)
+
+    def build(self) -> interfaces.plugins.PluginInterface:
+        """Build the context.
+        todo"""
+        plugin = self.plugin.get_full_path(self.os)
+        available_automagics = self.get_available_automagics()
+
+        automagics = automagic.choose_automagic(available_automagics, plugin)
+
+        os_stackers = automagic.stacker.choose_os_stackers(plugin)
+        self.context.config[self.KEY_STACKERS] = os_stackers
+
+        location = "file://" + self.dump_file.as_posix()
+        self.context.config[self.KEY_SINGLE_LOCATION] = location
+
+        constructed = construct_plugin(
+            self.context,
+            automagics,
+            plugin,
+            base_config_path,
+            None,
+            None,  # TODO: add file handler
+        )
+
+        return constructed
+
+    def get_available_automagics(self) -> List[interfaces.automagic.AutomagicInterface]:
+        """Returns a list of available automagics."""
+        return automagic.available(self.context)
 
 
 class Generic:
@@ -50,9 +124,31 @@ class Generic:
         """
         self.os = os
         self.plugins: List[PluginEntry] = self.get_all_plugins()
-        self.dump_file = dump_file
+        self.dump_file = self.validate_dump_file(dump_file)
+        self.context = Context(os, dump_file)
 
         logger.info(f"Generic OS initialized: {self.os}")
+
+    def __getattribute__(self, name: str) -> Any:
+        """Handle attribute acces for plugins."""
+        # todo
+
+    def run_plugin(self, plugin: PluginEntry, **kwargs: Any) -> Any:
+        """
+        Run a plugin with the given arguments.
+        """
+        # Create basic context
+
+        # Extend it with kwargs
+
+        # Run the plugin
+
+    def validate_dump_file(self, dump_file: Path) -> Path:
+        """Validates the dump file."""
+        if not dump_file.is_file():
+            raise FileNotFoundError(f"The file {dump_file} does not exist.")
+        # TODO: validate the dump file with volatility handlers
+        return dump_file
 
     def get_all_plugins(self) -> List[PluginEntry]:
         """Returns all plugins for the OS."""
