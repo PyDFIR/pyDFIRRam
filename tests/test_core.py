b90feb68db036ea2e22d0ce2b2f26e697dccad52
std3 <67806187+standard3@users.noreply.github.com>
2024-05-02 10:04:58 +0200

    test: generic_build

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,11 +2,27 @@ from pathlib import Path
 
 from pydfirram.core.base import Generic, OperatingSystem
 
+DUMP_FILE = Path("tests/data/dump.raw")
+
 
 def test_generic():
     os = OperatingSystem.WINDOWS
-    dumpfile = Path("tests/data/dump.raw")
+    dumpfile = Path(DUMP_FILE)
 
     generic = Generic(os, dumpfile)
 
     assert len(generic.plugins) > 0
+
+
+def test_generic_build():
+    os = OperatingSystem.WINDOWS
+    dumpfile = Path(DUMP_FILE)
+
+    generic = Generic(os, dumpfile)
+
+    plugin = generic.plugins[0]  # random plugin
+
+    generic.run_plugin(plugin)
+    assert generic.context is not None
+
+    generic.context.build()
